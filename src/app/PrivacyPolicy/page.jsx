b287e3fd157b005b2bf951b828f2c53ad5a882c6
stopthefake1 <100723557+stopthefake1@users.noreply.github.com>
2024-11-26 17:04:17 +0100

    Mise à jour des fichiers et exclusion de test-repo/

--- src/app/PrivacyPolicy/page.jsx
+++ src/app/PrivacyPolicy/page.jsx
@@ -1,85 +1,126 @@
-import React from 'react';
-import '../../styles/simulateur.css'
+import Image from 'next/image';
+import '../../styles/simulateur.css';
+
 const PrivacyPolicy = () => {
   return (
-    <><header className="merci-header">
-      <a href="/">
-        <img href="/" src="/images/logo-my-ohm.png" alt="Logo My ohm technologies" className="logo" />
-      </a>
+    <>
+      <header className="merci-header">
+        <a href="/">
+          <Image
+            src="/images/logo-my-ohm.png"
+            alt="Logo My ohm technologies"
+            className="logo"
+            width={100} // Remplacez par la largeur appropriée
+            height={50} // Remplacez par la hauteur appropriée
+          />
+        </a>
 
-      <div className="header-actions">
-        
-        <span className="phone-number">
-          <img src="/images/svg/material-symbols_call.svg" alt="phone" />
-          09 75 66  68 58</span>
-      </div>
-    </header><div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+        <div className="header-actions">
+          <span className="phone-number">
+            <Image
+              src="/images/svg/material-symbols_call.svg"
+              alt="phone"
+              width={24} // Remplacez par la largeur appropriée
+              height={24} // Remplacez par la hauteur appropriée
+            />
+            09 75 66 68 58
+          </span>
+        </div>
+      </header>
+
+      <div className="privacy-container">
         <h1>Politique de Confidentialité</h1>
         <p>
-          Chez <strong>VotreEntreprise</strong>, nous accordons une grande importance à la protection de votre vie privée.
-          Cette politique de confidentialité décrit les types d'informations que nous recueillons auprès de vous lorsque
-          vous visitez notre site Web, ainsi que la manière dont nous les utilisons et les protégeons.
+          Chez <strong>VotreEntreprise</strong>, nous accordons une grande importance à la protection de votre vie
+          privée. Cette politique de confidentialité décrit les types d&apos;informations que nous recueillons auprès de
+          vous lorsque vous visitez notre site Web, ainsi que la manière dont nous les utilisons et les protégeons.
         </p>
 
         <h2>1. Collecte des Informations</h2>
         <p>
-          Nous collectons différentes informations pour mieux répondre à vos besoins et améliorer votre expérience utilisateur.
-          Ces informations incluent :
+          Nous collectons différentes informations pour mieux répondre à vos besoins et améliorer votre expérience
+          utilisateur. Ces informations incluent :
         </p>
         <ul>
-          <li><strong>Informations personnelles :</strong> Nom, adresse e-mail, numéro de téléphone, etc., que vous nous fournissez lors de votre inscription, des demandes de devis ou de contact.</li>
-          <li><strong>Données de navigation :</strong> Adresse IP, type de navigateur, pages visitées et durée de visite, via des cookies et autres technologies similaires.</li>
+          <li>
+            <strong>Informations personnelles :</strong> Nom, adresse e-mail, numéro de téléphone, etc., que vous nous
+            fournissez lors de votre inscription, des demandes de devis ou de contact.
+          </li>
+          <li>
+            <strong>Données de navigation :</strong> Adresse IP, type de navigateur, pages visitées et durée de visite,
+            via des cookies et autres technologies similaires.
+          </li>
         </ul>
 
         <h2>2. Utilisation des Informations</h2>
         <p>Les informations recueillies sont utilisées pour :</p>
         <ul>
-          <li>Vous fournir les services demandés, comme les devis et simulations pour l’installation de panneaux photovoltaïques.</li>
+          <li>
+            Vous fournir les services demandés, comme les devis et simulations pour l’installation de panneaux
+            photovoltaïques.
+          </li>
           <li>Améliorer notre site Web et nos services en analysant les comportements de navigation.</li>
-          <li>Vous envoyer des informations promotionnelles concernant nos services, sous réserve de votre consentement.</li>
+          <li>
+            Vous envoyer des informations promotionnelles concernant nos services, sous réserve de votre consentement.
+          </li>
         </ul>
 
         <h2>3. Partage des Informations</h2>
         <p>
-          Nous ne vendons ni ne louons vos informations personnelles. Cependant, nous pouvons partager vos informations dans
-          les situations suivantes :
+          Nous ne vendons ni ne louons vos informations personnelles. Cependant, nous pouvons partager vos informations
+          dans les situations suivantes :
         </p>
         <ul>
-          <li>Avec des prestataires de services tiers pour le traitement de vos demandes ou pour améliorer nos services (ex. : analyse de données, hébergement du site Web).</li>
-          <li>Conformément à la loi, si cela est requis pour répondre à des procédures légales ou pour protéger nos droits et intérêts.</li>
+          <li>
+            Avec des prestataires de services tiers pour le traitement de vos demandes ou pour améliorer nos services
+            (ex. : analyse de données, hébergement du site Web).
+          </li>
+          <li>
+            Conformément à la loi, si cela est requis pour répondre à des procédures légales ou pour protéger nos droits
+            et intérêts.
+          </li>
         </ul>
 
         <h2>4. Cookies</h2>
         <p>
-          Notre site utilise des cookies pour améliorer votre expérience de navigation. Les cookies sont de petits fichiers
-          stockés sur votre appareil qui nous aident à analyser le trafic du site et à personnaliser notre contenu pour
-          mieux vous servir. Vous pouvez configurer votre navigateur pour refuser les cookies, mais cela pourrait limiter
-          votre accès à certaines fonctionnalités du site.
+          Notre site utilise des cookies pour améliorer votre expérience de navigation. Les cookies sont de petits
+          fichiers stockés sur votre appareil qui nous aident à analyser le trafic du site et à personnaliser notre
+          contenu pour mieux vous servir. Vous pouvez configurer votre navigateur pour refuser les cookies, mais cela
+          pourrait limiter votre accès à certaines fonctionnalités du site.
         </p>
 
         <h2>5. Sécurité des Données</h2>
         <p>
-          Nous mettons en œuvre des mesures de sécurité pour protéger vos informations personnelles contre tout accès non
-          autorisé, toute divulgation ou tout dommage. Cependant, aucune transmission de données sur Internet n'est totalement
-          sécurisée ; nous ne pouvons donc garantir une sécurité absolue.
+          Nous mettons en œuvre des mesures de sécurité pour protéger vos informations personnelles contre tout accès
+          non autorisé, toute divulgation ou tout dommage. Cependant, aucune transmission de données sur Internet
+          n&apos;est totalement sécurisée ; nous ne pouvons donc garantir une sécurité absolue.
         </p>
 
         <h2>6. Vos Droits</h2>
-        <p>
-          Conformément aux lois en vigueur, vous disposez des droits suivants :
-        </p>
+        <p>Conformément aux lois en vigueur, vous disposez des droits suivants :</p>
         <ul>
-          <li><strong>Accès :</strong> Vous pouvez demander à consulter les informations personnelles que nous détenons à votre sujet.</li>
-          <li><strong>Rectification :</strong> Vous pouvez corriger ou mettre à jour vos informations personnelles.</li>
-          <li><strong>Suppression :</strong> Vous pouvez demander la suppression de vos informations, dans les limites prévues par la loi.</li>
-          <li><strong>Retrait du consentement :</strong> Si vous avez donné votre consentement pour recevoir des informations marketing, vous pouvez le retirer à tout moment.</li>
+          <li>
+            <strong>Accès :</strong> Vous pouvez demander à consulter les informations personnelles que nous détenons à
+            votre sujet.
+          </li>
+          <li>
+            <strong>Rectification :</strong> Vous pouvez corriger ou mettre à jour vos informations personnelles.
+          </li>
+          <li>
+            <strong>Suppression :</strong> Vous pouvez demander la suppression de vos informations, dans les limites
+            prévues par la loi.
+          </li>
+          <li>
+            <strong>Retrait du consentement :</strong> Si vous avez donné votre consentement pour recevoir des
+            informations marketing, vous pouvez le retirer à tout moment.
+          </li>
         </ul>
 
         <h2>7. Modifications de la Politique de Confidentialité</h2>
         <p>
-          Nous nous réservons le droit de modifier cette politique de confidentialité. Toute modification sera publiée sur
-          cette page, avec une mise à jour de la date d'entrée en vigueur. Nous vous encourageons à consulter régulièrement
-          cette page pour rester informé de nos pratiques de confidentialité.
+          Nous nous réservons le droit de modifier cette politique de confidentialité. Toute modification sera publiée
+          sur cette page, avec une mise à jour de la date d&apos;entrée en vigueur. Nous vous encourageons à consulter
+          régulièrement cette page pour rester informé de nos pratiques de confidentialité.
         </p>
 
         <h2>8. Contact</h2>
@@ -88,14 +129,20 @@ const PrivacyPolicy = () => {
           veuillez nous contacter à :
         </p>
         <address>
-          <strong>Myohmtechnologies</strong><br />
-          Adresse de l'entreprise : 544 Av. Frédéric Mistral 04100 Manosque<br />
-          Téléphone : 09 75 66 68 58<br />
+          <strong>Myohmtechnologies</strong>
+          <br />
+          Adresse de l&apos;entreprise : 544 Av. Frédéric Mistral 04100 Manosque
+          <br />
+          Téléphone : 09 75 66 68 58
+          <br />
           Email : contact@myohmtechnologies.com
         </address>
 
-        <p>Numéro de SIRET : <strong>91760190800010</strong></p>
-      </div></>
+        <p>
+          Numéro de SIRET : <strong>91760190800010</strong>
+        </p>
+      </div>
+    </>
   );
 };
 
--- src/app/TermsAndConditions/page.jsx
+++ src/app/TermsAndConditions/page.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import '../../styles/simulateur.css'
+import Image from 'next/image';
+import '../../styles/simulateur.css';
 
 const TermsAndConditions = () => {
   return (
-    <><header className="merci-header">
-      <a href="/">
-        <img href="/" src="/images/logo-my-ohm.png" alt="Logo My ohm technologies" className="logo" />
-      </a>
+    <>
+      <header className="merci-header">
+        <a href="/">
+          <Image
+            src="/images/logo-my-ohm.png"
+            alt="Logo My ohm technologies"
+            className="logo"
+            width={100} // Remplacez par la largeur appropriée
+            height={50} // Remplacez par la hauteur appropriée
+          />
+        </a>
 
-      <div className="header-actions">
+        <div className="header-actions">
+          <span className="phone-number">
+            <Image
+              src="/images/svg/material-symbols_call.svg"
+              alt="phone"
+              width={24} // Remplacez par la largeur appropriée
+              height={24} // Remplacez par la hauteur appropriée
+            />
+            09 75 66 68 58
+          </span>
+        </div>
+      </header>
 
-        <span className="phone-number">
-          <img src="/images/svg/material-symbols_call.svg" alt="phone" />
-          09 75 66  68 58</span>
-      </div>
-    </header><div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+      <div className="terms-container">
         <h1>Termes et Conditions</h1>
-        <p>Bienvenue sur le site <strong>VotreEntreprise</strong>. En utilisant notre site, vous acceptez les termes et conditions ci-dessous. Veuillez les lire attentivement avant d'utiliser nos services.</p>
+        <p>
+          Bienvenue sur le site <strong>VotreEntreprise</strong>. En utilisant notre site, vous acceptez les termes et
+          conditions ci-dessous. Veuillez les lire attentivement avant d&apos;utiliser nos services.
+        </p>
 
         <h2>1. Acceptation des Termes</h2>
-        <p>En accédant à notre site Web et en utilisant nos services, vous acceptez d'être lié par les présents termes et conditions. Si vous n'êtes pas d'accord avec une partie de ces termes, vous ne devez pas utiliser notre site.</p>
+        <p>
+          En accédant à notre site Web et en utilisant nos services, vous acceptez d&apos;être lié par les présents termes et
+          conditions. Si vous n&apos;êtes pas d&apos;accord avec une partie de ces termes, vous ne devez pas utiliser notre site.
+        </p>
 
         <h2>2. Services Offerts</h2>
-        <p><strong>VotreEntreprise</strong> fournit des informations, des simulations et des services liés à l’installation de panneaux photovoltaïques. Nos services sont destinés à vous aider dans votre transition vers l’énergie solaire et à vous fournir des estimations et des conseils adaptés à vos besoins énergétiques.</p>
+        <p>
+          <strong>VotreEntreprise</strong> fournit des informations, des simulations et des services liés à l’installation de
+          panneaux photovoltaïques. Nos services sont destinés à vous aider dans votre transition vers l’énergie solaire et à
+          vous fournir des estimations et des conseils adaptés à vos besoins énergétiques.
+        </p>
 
         <h2>3. Utilisation du Site</h2>
         <p>Vous vous engagez à utiliser notre site uniquement à des fins légales et dans le respect des lois en vigueur. Il est interdit de :</p>
@@ -33,37 +57,71 @@ const TermsAndConditions = () => {
         </ul>
 
         <h2>4. Propriété Intellectuelle</h2>
-        <p>Tout le contenu présent sur ce site, incluant les textes, images, logos et graphiques, est protégé par des droits d'auteur et appartient à <strong>VotreEntreprise</strong> ou à ses partenaires. Vous n'êtes pas autorisé à reproduire, distribuer ou modifier ce contenu sans notre permission expresse.</p>
+        <p>
+          Tout le contenu présent sur ce site, incluant les textes, images, logos et graphiques, est protégé par des droits
+          d&apos;auteur et appartient à <strong>VotreEntreprise</strong> ou à ses partenaires. Vous n&apos;êtes pas autorisé à
+          reproduire, distribuer ou modifier ce contenu sans notre permission expresse.
+        </p>
 
         <h2>5. Limitations de Responsabilité</h2>
-        <p>Nous nous efforçons de fournir des informations exactes et à jour. Cependant, <strong>VotreEntreprise</strong> ne garantit pas l’exactitude, la complétude ou la pertinence des informations publiées sur le site. Nous déclinons toute responsabilité pour tout dommage résultant de l’utilisation ou de l’incapacité à utiliser notre site ou nos services.</p>
+        <p>
+          Nous nous efforçons de fournir des informations exactes et à jour. Cependant, <strong>VotreEntreprise</strong> ne
+          garantit pas l&apos;exactitude, la complétude ou la pertinence des informations publiées sur le site. Nous déclinons
+          toute responsabilité pour tout dommage résultant de l&apos;utilisation ou de l&apos;incapacité à utiliser notre site ou nos
+          services.
+        </p>
 
         <h2>6. Politique de Confidentialité</h2>
-        <p>Votre utilisation de notre site est également soumise à notre <a href="/politique-de-confidentialite">Politique de Confidentialité</a>, qui explique comment nous collectons et utilisons vos informations personnelles.</p>
+        <p>
+          Votre utilisation de notre site est également soumise à notre{' '}
+          <a href="/politique-de-confidentialite">Politique de Confidentialité</a>, qui explique comment nous collectons et
+          utilisons vos informations personnelles.
+        </p>
 
         <h2>7. Modifications des Termes et Conditions</h2>
-        <p>Nous nous réservons le droit de modifier ces termes et conditions à tout moment. Les changements seront publiés sur cette page, et nous vous encourageons à les consulter régulièrement pour rester informé. En continuant d'utiliser notre site après la mise à jour des termes, vous acceptez les modifications.</p>
+        <p>
+          Nous nous réservons le droit de modifier ces termes et conditions à tout moment. Les changements seront publiés sur
+          cette page, et nous vous encourageons à les consulter régulièrement pour rester informé. En continuant d&apos;utiliser
+          notre site après la mise à jour des termes, vous acceptez les modifications.
+        </p>
 
         <h2>8. Liens vers des Sites Tiers</h2>
-        <p>Notre site peut contenir des liens vers des sites externes. Ces liens sont fournis pour votre commodité, mais nous n’avons aucun contrôle sur ces sites et déclinons toute responsabilité quant à leur contenu ou leurs politiques. Veuillez consulter les conditions d’utilisation et politiques de confidentialité de tout site tiers.</p>
+        <p>
+          Notre site peut contenir des liens vers des sites externes. Ces liens sont fournis pour votre commodité, mais nous
+          n’avons aucun contrôle sur ces sites et déclinons toute responsabilité quant à leur contenu ou leurs politiques.
+          Veuillez consulter les conditions d&apos;utilisation et politiques de confidentialité de tout site tiers.
+        </p>
 
         <h2>9. Résiliation</h2>
-        <p>Nous nous réservons le droit de restreindre ou de résilier votre accès au site si vous enfreignez ces termes et conditions, ou si nous suspectons une utilisation inappropriée de nos services. En cas de résiliation, toutes les limitations de responsabilité prévues dans ces termes restent applicables.</p>
+        <p>
+          Nous nous réservons le droit de restreindre ou de résilier votre accès au site si vous enfreignez ces termes et
+          conditions, ou si nous suspectons une utilisation inappropriée de nos services. En cas de résiliation, toutes les
+          limitations de responsabilité prévues dans ces termes restent applicables.
+        </p>
 
         <h2>10. Loi Applicable et Juridiction</h2>
-        <p>Ces termes et conditions sont régis par les lois en vigueur dans le pays où est situé <strong>VotreEntreprise</strong>. Tout litige lié à l'utilisation de notre site sera soumis à la juridiction des tribunaux compétents.</p>
+        <p>
+          Ces termes et conditions sont régis par les lois en vigueur dans le pays où est situé <strong>VotreEntreprise</strong>.
+          Tout litige lié à l&apos;utilisation de notre site sera soumis à la juridiction des tribunaux compétents.
+        </p>
 
         <h2>11. Contact</h2>
         <p>Pour toute question ou demande d’information concernant ces termes et conditions, vous pouvez nous contacter à :</p>
         <address>
-          <strong>Myohmtechnologies</strong><br />
-          Adresse de l'entreprise : 544 Av. Frédéric Mistral 04100 Manosque<br />
-          Téléphone : 09 75 66 68 58<br />
+          <strong>Myohmtechnologies</strong>
+          <br />
+          Adresse de l&apos;entreprise : 544 Av. Frédéric Mistral 04100 Manosque
+          <br />
+          Téléphone : 09 75 66 68 58
+          <br />
           Email : contact@myohmtechnologies.com
         </address>
 
-        <p>numéro de SIRET : <strong>91760190800010</strong></p>
-      </div></>
+        <p>
+          numéro de SIRET : <strong>91760190800010</strong>
+        </p>
+      </div>
+    </>
   );
 };
 
