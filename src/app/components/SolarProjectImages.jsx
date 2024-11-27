b8189b6251c9c44ada0aa58b3436b01a40f28754
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-11-27 18:52:40 +0100

    Update SolarProjectImages.jsx

--- src/app/components/SolarProjectImages.jsx
+++ src/app/components/SolarProjectImages.jsx
@@ -1,31 +1,27 @@
 // src/app/components/SolarProjectImages.jsx
 
-<<<<<<< HEAD
-=======
 import Image from "next/image";
 
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
+/**
+ * A React component that displays a series of solar project images.
+ * 
+ * @param {Object} props - The properties object.
+ * @param {string[]} props.images - An array of image URLs. The first image
+ *   will be displayed in a larger format, and the next two images will be 
+ *   displayed in a smaller format.
+ */
 const SolarProjectImages = ({ images }) => {
   return (
     <section className="solar-project-images">
       <div className="image-grid">
         <div className="image-large">
           {/* Afficher la première image comme grande */}
-<<<<<<< HEAD
-          <img src={images[0]} alt="Installation de panneaux solaires" />
-        </div>
-        <div className="image-small">
-          {/* Afficher les deux autres images comme petites */}
-          <img src={images[1]} alt="Maison avec panneaux solaires" />
-          <img src={images[2]} alt="Ferme de panneaux solaires" />
-=======
           <Image src={images[0]} alt="Installation de panneaux solaires" />
         </div>
         <div className="image-small">
           {/* Afficher les deux autres images comme petites */}
           <Image src={images[1]} alt="Maison avec panneaux solaires" />
           <Image src={images[2]} alt="Ferme de panneaux solaires" />
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
         </div>
       </div>
     </section>
