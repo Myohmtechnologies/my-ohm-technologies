4817ba383b1a5dc77be51397f9684dcf101bbb53
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-11-27 18:57:46 +0100

    Update RealisationSection.jsx

--- src/app/components/RealisationSection.jsx
+++ src/app/components/RealisationSection.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const RealisationSection = () => {
     return (
       <section className="realisation-section">
@@ -8,23 +10,29 @@ const RealisationSection = () => {
         <div className="cards-container">
           <div className="realisation-cards">
             <div className="realisation-card">
-              <img
+              <Image
                 src="/images/PHOTO-2024-05-22-17-39-06.jpg"
                 alt="Réalisation 1"
+                width={500}
+                height={500}                
               />
               <h3 className="realisation-text">Installation de panneaux photovoltaïques</h3>
             </div>
             <div className="realisation-card">
-              <img
+              <Image
                 src="/images/PHOTO-2024-05-22-17-42-57.jpg"
                 alt="Réalisation 2"
+                width={500}
+                height={500}    
               />
               <p className="realisation-text">Installation de panneaux photovoltaïques</p>
             </div>
             <div className="realisation-card">
-              <img
+              <Image
                 src="/images/IMG_5056.jpg"
                 alt="Réalisation 3"
+                width={500}
+                height={500}    
               />
               <p className="realisation-text">Installation de panneaux photovoltaïques</p>
             </div>
