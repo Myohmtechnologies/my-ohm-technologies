eaf2aad63f1134516504176336008722bef57886
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-11-27 19:05:55 +0100

    Update CustomSolutions.jsx

--- src/app/components/CustomSolutions.jsx
+++ src/app/components/CustomSolutions.jsx
@@ -1,4 +1,5 @@
 // src/app/components/CustomSolutions.jsx
+import Image from 'next/image';
 import '../../styles/globals.css'
 const CustomSolutions = ({ installation }) => {
   return (
@@ -7,7 +8,7 @@ const CustomSolutions = ({ installation }) => {
         <div className="section-header">
           <h1>Solutions Personnalisées pour Chaque Projet</h1>
           <p className="description">
-            Chez [Nom de l’Entreprise], nous concevons et installons des systèmes énergétiques sur mesure
+            Chez , nous concevons et installons des systèmes énergétiques sur mesure
             qui répondent aux besoins spécifiques de nos clients.
           </p>
         </div>
@@ -16,19 +17,19 @@ const CustomSolutions = ({ installation }) => {
           <h2>Les caractéristiques</h2>
           <div className="details">
             <p className="detail-p">
-              <img src="images/icone-nos-realisation/iconamoon_home-fill.svg" alt="Maison icon" />
+              <Image src="images/icone-nos-realisation/iconamoon_home-fill.svg" alt="Maison icon" width={30} height={30}/>
               Maison: {installation.surfaceMaison}m², consommation de {installation.surfaceMaison * 100} kWh/an
             </p>
             <p className="detail-p">
-              <img src="/images/icone-nos-realisation/mdi_thunder.svg" alt="Puissance icon" />
+              <Image src="/images/icone-nos-realisation/mdi_thunder.svg" alt="Puissance icon" width={30} height={30}/>
               Puissance: {installation.puissanceMax} kWc
             </p>
             <p className="detail-p">
-              <img src="/images/icone-nos-realisation/mingcute_solar-panel-fill.svg" alt="Panneaux icon" />
+              <Image src="/images/icone-nos-realisation/mingcute_solar-panel-fill.svg" alt="Panneaux icon" width={30} height={30}/>
               Panneaux: {installation.nombrePanneaux} panneaux {installation.marquePanneaux}
             </p>
             <p className="detail-p">
-              <img src="/images/icone-nos-realisation/simple-icons_wire.svg" alt="Onduleur icon" />
+              <Image src="/images/icone-nos-realisation/simple-icons_wire.svg" alt="Onduleur icon"width={30} height={30} />
               Onduleur: {installation.marqueMicroOnduleur}
             </p>
           </div>
