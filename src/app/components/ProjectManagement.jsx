b4aeb5c039fd5137309b4d7739ab5bf9dfea1206
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-11-27 19:00:27 +0100

    Update ProjectManagement.jsx

--- src/app/components/ProjectManagement.jsx
+++ src/app/components/ProjectManagement.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 // src/app/components/ProjectManagement.jsx
 const ProjectManagement = () => {
     return (
@@ -6,58 +8,44 @@ const ProjectManagement = () => {
         <div className="content-wrapper">
           <div className="article-list">
             <article className="small-article">
-              <img src="/images/blog/Image (1).png" alt="Matériaux Innovants" />
+              <Image src="/images/blog/Image (1).png" alt="Matériaux Innovants"  width={500}/>
               <div className="article-content">
                 <span className="tag">Écologique</span>
                 <h3>Matériaux Innovants pour une Construction Moderne</h3>
                 <p>
                   Le secteur de la construction connaît une révolution grâce à
-<<<<<<< HEAD
-                  l'utilisation de matériaux...
-=======
                   l&apos;utilisation de matériaux...
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
                 </p>
                 <span className="author-date">PHOENIX BAKER • 19 JAN 2022</span>
               </div>
             </article>
             <article className="small-article">
-              <img
+              <Image
                 src="/images/blog/Image.png"
                 alt="Importance de la Construction Éco Responsable"
+                width={500}
               />
               <div className="article-content">
                 <span className="tag">Écologique</span>
-<<<<<<< HEAD
-                <h3>L'Importance de la Construction Éco Responsable</h3>
-                <p>
-                  Construire de manière durable n'est plus une option, c'est une
-=======
                 <h3>L&apos;Importance de la Construction Éco Responsable</h3>
                 <p>
                   Construire de manière durable n&apos;est plus une option, c&apos;est une
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
                   nécessité...
                 </p>
                 <span className="author-date">PHOENIX BAKER • 19 JAN 2022</span>
               </div>
             </article>
             <article className="small-article">
-              <img
+              <Image
                 src="/images/blog/Image.png"
                 alt="Importance de la Construction Éco Responsable"
+                width={500}
               />
               <div className="article-content">
                 <span className="tag">Écologique</span>
-<<<<<<< HEAD
-                <h3>L'Importance de la Construction Éco Responsable</h3>
-                <p>
-                  Construire de manière durable n'est plus une option, c'est une
-=======
                 <h3>L&apos;Importance de la Construction Éco Responsable</h3>
                 <p>
                   Construire de manière durable n&apos;est plus une option, c&apos;est une
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
                   nécessité...
                 </p>
                 <span className="author-date">PHOENIX BAKER • 19 JAN 2022</span>
@@ -65,21 +53,16 @@ const ProjectManagement = () => {
             </article>
           </div>
           <div className="featured-article">
-            <img
+            <Image
               src="/images/blog/Rectangle 13.png"
               alt="Construction Éco Responsable"
+              width={500}
             />
             <div className="article-content">
               <span className="tag">Écologique</span>
-<<<<<<< HEAD
-              <h3>L'Importance de la Construction Éco-Responsable</h3>
-              <p>
-                Construire de manière durable n'est plus une option, c'est une
-=======
               <h3>L&apos;Importance de la Construction Éco-Responsable</h3>
               <p>
                 Construire de manière durable n&apos;est plus une option, c&apos;est une
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
                 nécessité. Nous adoptons une approche éco-responsable à chaque
                 étape du processus...
               </p>
