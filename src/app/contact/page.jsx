84b10b3e5547ad277c51a7eab0107f6d7d30dafd
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-11-28 10:24:35 +0100

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -13,7 +13,7 @@ export default function Simulation() {
     email: '',
     phone: '',
   });
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -36,30 +36,28 @@ export default function Simulation() {
   };
 
   return (
-    <><Header /><div>
-<<<<<<< HEAD
-          <h1>Simulation d'Énergie</h1>
-=======
-          <h1>Simulation d&apos;Énergie</h1>
->>>>>>> 642d8a2 (Réinitialisation du dépôt)
-          <p>Merci de renseigner vos informations pour être contacté par un commercial.</p>
+    <>
+      <Header />
+      <div>
+        <h1>Simulation d&apos;Énergie</h1>
+        <p>Merci de renseigner vos informations pour être contacté par un commercial.</p>
 
-          <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', maxWidth: '400px' }}>
-              <label>Nom :</label>
-              <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', maxWidth: '400px' }}>
+          <label>Nom :</label>
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
 
-              <label>Email :</label>
-              <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+          <label>Email :</label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
 
-              <label>Téléphone :</label>
-              <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
+          <label>Téléphone :</label>
+          <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
 
-              <button type="submit" style={{ marginTop: '10px', padding: '10px', background: '#4CAF50', color: 'white' }}>
-                  Envoyer
-              </button>
-          </form>
+          <button type="submit" style={{ marginTop: '10px', padding: '10px', background: '#4CAF50', color: 'white' }}>
+            Envoyer
+          </button>
+        </form>
       </div>
-      <Footer/>
-      </>
+      <Footer />
+    </>
   );
 }
