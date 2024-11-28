c8f495dba372f2e43610c218422163e848108580
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-11-28 11:23:11 +0100

    Update PourquoiSolaireSection.jsx

--- src/app/components/PourquoiSolaireSection.jsx
+++ src/app/components/PourquoiSolaireSection.jsx
@@ -8,7 +8,8 @@ export default function PourquoiSolaireSection() {
         </div>
         <div className="why-div">
           <div>
-            <img src="images/évolution-du-tarif-délectricité.png" alt="Évolution du tarif de l'électricité" />
+            <Image src="images/évolution-du-tarif-délectricité.png" alt="Évolution du tarif de l'électricité" width={500}
+           height={150}/>
             <h3 style={{ marginBottom: '10px' }}>Économies sur vos factures:</h3>
             <p>
               L&apos;énergie solaire vous permet de réduire considérablement vos dépenses en électricité en produisant votre propre énergie
@@ -16,7 +17,8 @@ export default function PourquoiSolaireSection() {
             </p>
           </div>
           <div>
-            <img src="images/energie-verte-et-renouvelable.png" alt="Énergie verte et renouvelable" />
+            <Image src="images/energie-verte-et-renouvelable.png" alt="Énergie verte et renouvelable" width={500}
+           height={150}/>
             <h3 style={{ marginBottom: '10px' }}>Énergie verte et renouvelable:</h3>
             <p>
               En utilisant le solaire, vous réduisez votre empreinte carbone et contribuez à la protection de l&apos;environnement en optant
